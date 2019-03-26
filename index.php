f1a3d1e20c188dcfcfea7e99b4894efc2b9bf243
Salvador Sastré <chavosastre@gmail.com>
2019-03-26 15:11:09 -0600

    Cambios de prueba

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
   <head>
       <meta charset="utf-8">
       <title>Titulo de página</title>
-      <!-- Bootstrap core CSS-->
-      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-      <link href="css/styles.css" rel="stylesheet">
   </head>
   <body >
   pruebas
